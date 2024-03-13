a932dfdfef1f6de958e0952c2b0021ba8ba2c7c8
Yudi <12040397+Yudi@users.noreply.github.com>
2024-03-13 15:29:33 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'banco-questoes',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
